7f9b1b51ca2b808cad735e38a8da69c41e18a7e3
ANDY MILLS <SM21BM@FSU.EDU>
2023-10-02 09:54:51 -0400

    9:54 10/2 check point

--- sketch.js
+++ sketch.js
@@ -1,57 +1,98 @@
 //establishing columns
-let column1Div;
-let column2Div;
-let column3Div;
-let column4Div;
-let column5Div;
-let column6Div;
-let column7Div;
-//esablishing column content
-let submarine = ['I', 'WAS', 'FRESH', ' ', 'BUT', 'STILL', 'INHABITING']
-let trespassers = ['HAVE', 'YOUR', 'CHILDREN', 'FOLLOWED', 'IT', 'ANY', 'BETTER']
-let bystander = ['SET', 'PLEASURE', 'ASIDE', 'EVERY', ' ', 'BRANCH', 'BREAK']
-let fumes = ['MY', 'BETTER', 'HALF', 'SWEETHEART', "WASN'T", 'THAT', 'REVEALING']
-let soldiers = ["EYES", " ", "FORMED", "UNDER", "VILLAINOUS", "TOUCHES", " "]
-let derailment = ["INTO", "YESTERDAY", "BROKEN", "GROUND", "AT", "THE WINDOW", "PANE"]
-let baby = ["YOU", "HONEY", "THINGS", " ", "LEAST", "YOU'RE", "QUICK"]
+// let column1Div;
+// let column2Div;
+// let column3Div;
+// let column4Div;
+// let column5Div;
+// let column6Div;
+// let column7Div;
+// //esablishing column content
+// let submarine = ['I', 'WAS', 'FRESH', ' ', 'BUT', 'STILL', 'INHABITING']
+// let trespassers = ['HAVE', 'YOUR', 'CHILDREN', 'FOLLOWED', 'IT', 'ANY', 'BETTER']
+// let bystander = ['SET', 'PLEASURE', 'ASIDE', 'EVERY', ' ', 'BRANCH', 'BREAK']
+// let fumes = ['MY', 'BETTER', 'HALF', 'SWEETHEART', "WASN'T", 'THAT', 'REVEALING']
+// let soldiers = ["EYES", " ", "FORMED", "UNDER", "VILLAINOUS", "TOUCHES", " "]
+// let derailment = ["INTO", "YESTERDAY", "BROKEN", "GROUND", "AT", "THE WINDOW", "PANE"]
+// let baby = ["YOU", "HONEY", "THINGS", " ", "LEAST", "YOU'RE", "QUICK"]
 
-//establishing button labels
-// let txt, word, keywords = ["SUBMARINE", "TRESPASSERS", "BYSTANDERS", "FUMES", "SOLDIERS", "DERAILMENT", "BABY"
-// ]
+// //establishing button things
 
-function setup() {
-  noCanvas();
 
-  // createButtons();
-}
+// //establishing button labels
+// // let txt, word, keywords = ["SUBMARINE", "TRESPASSERS", "BYSTANDERS", "FUMES", "SOLDIERS", "DERAILMENT", "BABY"
+// // ]
 
-function draw() {
-  //drawing columns
-  column1Div = select("#column1");
-  column2Div = select("#column2");
-  column3Div = select("#column3");
-  column4Div = select("#column4");
-  column5Div = select("#column5");
-  column6Div = select("#column6");
-  column7Div = select("#column7");
-  //drawing comlumn content
-  column1Div.html(join(submarine, " "));
-  column2Div.html(join(trespassers, " "));
-  column3Div.html(join(bystander, " "));
-  column4Div.html(join(fumes, " "));
-  column5Div.html(join(soldiers, " "));
-  column6Div.html(join(derailment, " "));
-  column7Div.html(join(baby, " "));
-}
+// function setup() {
+//   noCanvas();
+// }
+
+// function draw() {
+//   //drawing columns
+//   column1Div = select("#column1");
+//   column2Div = select("#column2");
+//   column3Div = select("#column3");
+//   column4Div = select("#column4");
+//   column5Div = select("#column5");
+//   column6Div = select("#column6");
+//   column7Div = select("#column7");
+//   //drawing comlumn content
+//   column1Div.html(join(submarine, " "));
+//   column2Div.html(join(trespassers, " "));
+//   column3Div.html(join(bystander, " "));
+//   column4Div.html(join(fumes, " "));
+//   column5Div.html(join(soldiers, " "));
+//   column6Div.html(join(derailment, " "));
+//   column7Div.html(join(baby, " "));
+// }
 
 // function createButtons() {
-//   let buttons = []. buttonsW = 0, gap = 10;
-//   for (let i = 0; i <keywords.length; i++) {
-//     let button = createButton(keywords[i]);
-//     console.log(button.elt.textContent);
-//     button.class("button");
-//     button.style('color', keywords[i] == word ? 'rgb(200,0,0' : 'black');
-//     buttonsW += button.width;
-//     buttons.push(button);
 //   }
-// }
+
+
+//class test
+
+let submarine = ['I', 'WAS', 'FRESH', '&nbsp;', 'BUT', 'STILL', 'INHABITING']
+let trespassers = ['HAVE', 'YOUR', 'CHILDREN', 'FOLLOWED', 'IT', 'ANY', 'BETTER']
+let bystander = ['SET', 'PLEASURE', 'ASIDE', 'EVERY', '&nbsp;', 'BRANCH', 'BREAK']
+
+let index = 0;
+
+function setup() {
+  noCanvas()
+
+  //column 1
+  let button = createButton("SUBMARINE");
+  button.mouseClicked(mite);
+
+  //column2
+  let button2 = createButton("TRESPASSERS");
+  button.mouseClicked(cricket);
+
+  //column3
+  let button3 = createButton("BYSTANDER");
+  button.mouseClicked(moth);
+  }
+
+  //column1
+function mite(){
+  //add one to index
+  createP( submarine[index] );
+  index++;
+  console.log("Column 1 functional!!")
+}
+
+//column2
+function cricket(){
+  createP (trespassers[index]);
+  index++;
+  console.log("Column 2 functional!")
+}
+
+//column3
+function moth(){
+  createP (bystander[index]);
+  index++;
+  console.log("Column 3 functional!")
+}
+
+// //1 make button 2button.mouseClicked(fn) <-- make paragraph createP()
